d3ae14e847049696b60d68006082f6d7b91618d3
Samson Yeung <samson_gh@onepatchdown.net>
2016-07-21 00:58:41 +0200

    Cleanup oscrecv.py.
    pep8-ize oscrecv.py by dropping CamelCase for function names and other
    misc cleanup.

--- oscrecv.py
+++ oscrecv.py
@@ -1,30 +1,37 @@
-import lighthouse
+#!/usr/bin/env python 
+
+# stdlib
 import types
+
+# dependent libraries
 from OSC import OSCServer
 
+# Local libraries
+import lighthouse
 
-recvPort = 7000
-
+recv_port = 7000
 
 class ServerLighthouse(object):
 
-    def __init__(self, address='192.168.1.145', recvPort=recvPort):
+    def __init__(self, address='192.168.1.145', recv_port=recv_port):
 
         self.address = address
-        self.recvPort = recvPort
+        self.recv_port = recv_port
+
+        self.timed_out = False
 
         # Setup a reciever for OSC.
-        self.server = OSCServer((self.address, self.recvPort))
+        self.server = OSCServer((self.address, self.recv_port))
         self.server.timeout = 0
-        self.server.handleTimeout = types.MethodType(self.handleTimeout, self.server)
+        self.server.handle_timeout = types.MethodType(ServerLighthouse.handle_timeout, self)
 
         # Startup light
-        self.intitializeLight()
+        self.intitialize_light()
 
-    def handleTimeout(self):
-        self.timedOut = True
+    def handle_timeout(self):
+        self.timed_out = True
 
-    def eachFrame(self):
+    def each_frame(self):
         # clear timed_out flag
         self.server.timed_out = False
 
@@ -32,57 +39,51 @@ class ServerLighthouse(object):
         while not self.server.timed_out:
             self.server.handle_request()
 
-    def handleEvent(self, address, functionToCall):
-        def internalFunction(path, tags, args, source):
+    def handle_event(self, address, function):
+        def internal_function(path, tags, args, source):
             arg = int(args[0])
-            functionToCall(arg)
-
-        def internalFunctionZ(path, tags, args, source):
-            pass
-
-        self.server.addMsgHandler(address, internalFunction)
-        self.server.addMsgHandler('%s/z' % address, internalFunctionZ)
-
+            function(arg)
+        self.server.addMsgHandler(address, internal_function)
 
 class LighthouseMotion(ServerLighthouse):
 
     def __init__(self):
         super(LighthouseMotion, self).__init__()
 
-    def intitializeLight(self):
+    def intitialize_light(self):
         # Allow lamp to move at 25% speed.
 
         self.light = lighthouse.Lighthouse()
         self.light.set_speed(25)  # set speed to quarter AKA Rabtule (rabbit turtle)
 
-    def panLight(self, address):
-        self.handleEvent(address, self.light.set_pan_position)
+    def pan_light(self, address):
+        self.handle_event(address, self.light.set_pan_position)
         return 'pan'
 
-    def tiltLight(self, address):
-        self.handleEvent(address, self.light.set_tilt)
+    def tilt_light(self, address):
+        self.handle_event(address, self.light.set_tilt)
         return 'tilt'
 
-    def setSpeed(self, address):
-        self.handleEvent(address, self.light.set_speed)
+    def set_speed(self, address):
+        self.handle_event(address, self.light.set_speed)
         return 'speed'
 
-    def lightOnOff(self, address):
-        self.handleEvent(address, self.light.set_lamp)
+    def light_on_off(self, address):
+        self.handle_event(address, self.light.set_lamp)
 
-    def setBrightness(self, address):
-        self.handleEvent(address, self.light.set_lamp)
+    def set_brightness(self, address):
+        self.handle_event(address, self.light.set_lamp)
 
-    def setStrobe(self, address):
-        self.handleEvent(address, self.light.set_strobe)
+    def set_strobe(self, address):
+        self.handle_event(address, self.light.set_strobe)
 
 if __name__ == "__main__":
     light = LighthouseMotion()
-    light.panLight('/1/pan')
-    light.tiltLight('/1/tilt')
-    light.setSpeed('/1/speed')
-    light.lightOnOff('/1/lightControl')
-    light.setBrightness('/1/brightness')
-    light.setStrobe('/1/strobe')
+    light.pan_light('/1/pan')
+    light.tilt_light('/1/tilt')
+    light.set_speed('/1/speed')
+    light.light_on_off('/1/lightcontrol')
+    light.set_brightness('/1/brightness')
+    light.set_strobe('/1/strobe')
     while True:
-        light.eachFrame()
+        light.each_frame()
